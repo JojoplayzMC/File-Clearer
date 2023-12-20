ead9fa40e625262666cd4b719314bdbe7dd09088
JojoplayzMC <129040463+JojoplayzMC@users.noreply.github.com>
2023-12-20 13:29:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
 import os #line:1
-import tkinter as tk #line:4
-from tkinter import messagebox #line:5
-from tkinter import ttk #line:6
-from datetime import datetime ,timedelta #line:7
+import tkinter as tk #line:2
+from send2trash import send2trash #line:3
+from tkinter import messagebox #line:4
+from tkinter import ttk #line:5
+from datetime import datetime ,timedelta #line:6
 def clear_files ():#line:8
-    O00000O000O00O000 =entry_directory .get ()#line:9
-    O0OO00O00OO0O00O0 =entry_extensions .get ().split (',')#line:10
-    OO0O000OOO0OO0O00 =entry_date .get ()#line:11
-    if not os .path .exists (O00000O000O00O000 ):#line:12
-        messagebox .showerror ("Error","Directory does not exist.")#line:13
-        return #line:14
-    def O0O00000O0O000O0O (OOOO0OO0000OO000O ):#line:15
-        return any (OOOO0OO0000OO000O .lower ().endswith (OO0OO0OOOOO0O0OO0 .strip ())for OO0OO0OOOOO0O0OO0 in O0OO00O00OO0O00O0 )#line:16
+    O00O0000OO0O0000O =entry_directory .get ()#line:9
+    OOOOOO00OO0OOO000 =entry_extensions .get ().split (',')#line:10
+    OOO0OO0OO0O0OO00O =entry_date .get ()#line:11
+    if not os .path .exists (O00O0000OO0O0000O ):#line:13
+        messagebox .showerror ("Error","Directory does not exist.")#line:14
+        return #line:15
     try :#line:17
-        OOO0O00OOOOO0OOO0 =datetime .strptime (OO0O000OOO0OO0O00 ,'%d/%m/%Y')#line:18
-        OOO000O00O000O0OO =OOO0O00OOOOO0OOO0 +timedelta (days =1 )#line:19
+        O0O0000O000O0OO0O =datetime .strptime (OOO0OO0OO0O0OO00O ,'%m/%d/%Y')#line:18
+        OOO000OO000000O00 =O0O0000O000O0OO0O +timedelta (days =1 )#line:19
     except ValueError :#line:20
-        messagebox .showerror ("Error","Invalid date format. Use DD/MM/YYYY.")#line:21
+        messagebox .showerror ("Error","Invalid date format. Use MM/DD/YYYY.")#line:21
         return #line:22
-    for O00OOOO0000O000O0 in os .listdir (O00000O000O00O000 ):#line:23
-        O0O000000OOO0O000 =os .path .join (O00000O000O00O000 ,O00OOOO0000O000O0 )#line:24
-        if not os .path .isdir (O0O000000OOO0O000 )and not O0O00000O0O000O0O (O0O000000OOO0O000 ):#line:25
-            O0O0O00O0O0OO0O0O =datetime .fromtimestamp (os .path .getctime (O0O000000OOO0O000 ))#line:26
-            if O0O0O00O0O0OO0O0O <OOO000O00O000O0OO :#line:27
-                try :#line:28
-                    os .remove (O0O000000OOO0O000 )#line:29
-                    text .insert (tk .END ,f"Deleted: {O0O000000OOO0O000}\n")#line:30
-                except Exception as O0000OO0000O0O00O :#line:31
-                    text .insert (tk .END ,f"Error deleting {O0O000000OOO0O000}: {O0000OO0000O0O00O}\n")#line:32
-                    text .see (tk .END )#line:33
-root =tk .Tk ()#line:34
-root .title ("File Clearer")#line:35
-label_directory =tk .Label (root ,text ="Enter the directory path:")#line:36
-label_directory .pack ()#line:37
-entry_directory =tk .Entry (root )#line:38
-entry_directory .pack ()#line:39
-label_extensions =tk .Label (root ,text ="Enter safe file extensions and seperate file extentions with a comma (,):")#line:40
-label_extensions .pack ()#line:41
-entry_extensions =tk .Entry (root )#line:42
-entry_extensions .insert (0 ,'.safe')#line:43
-entry_extensions .pack ()#line:44
-label_date =tk .Label (root ,text ="Enter date (MM/DD/YYYY) to delete files created before:")#line:45
-label_date .pack ()#line:46
-default_date =datetime .now ().strftime ('%m/%d/%Y')#line:47
-entry_date =tk .Entry (root )#line:48
-entry_date .insert (0 ,default_date )#line:49
-entry_date .pack ()#line:50
-button =tk .Button (root ,text ="Clear Files",command =clear_files )#line:51
-button .pack ()#line:52
-text =tk .Text (root ,height =10 ,width =40 )#line:53
-text .pack ()#line:54
-root .mainloop ()
+    O00O00000OOO00OO0 =delete_checkbox_var .get ()#line:24
+    for OOOO000OO0OO0OO00 in os .listdir (O00O0000OO0O0000O ):#line:26
+        O0O0000O0OOO0000O =os .path .join (O00O0000OO0O0000O ,OOOO000OO0OO0OO00 )#line:27
+        if not os .path .isdir (O0O0000O0OOO0000O )and not any (OOOO000OO0OO0OO00 .lower ().endswith (OO0O0000OO0OOO0O0 .strip ())for OO0O0000OO0OOO0O0 in OOOOOO00OO0OOO000 ):#line:29
+            O00OOOO0OO0OOO0O0 =datetime .fromtimestamp (os .path .getctime (O0O0000O0OOO0000O ))#line:30
+            if O00OOOO0OO0OOO0O0 <OOO000OO000000O00 :#line:32
+                try :#line:33
+                    if O00O00000OOO00OO0 :#line:34
+                        os .remove (O0O0000O0OOO0000O )#line:35
+                    else :#line:36
+                        send2trash (O0O0000O0OOO0000O )#line:37
+                    text .insert (tk .END ,f"Deleted: {O0O0000O0OOO0000O}\n")#line:39
+                except Exception as O000OO00O00O00O0O :#line:40
+                    text .insert (tk .END ,f"Error deleting {O0O0000O0OOO0000O}: {O000OO00O00O00O0O}\n")#line:41
+    text .see (tk .END )#line:43
+root =tk .Tk ()#line:45
+root .title ("File Clearer")#line:46
+label_directory =tk .Label (root ,text ="Enter the directory path:")#line:48
+label_directory .pack ()#line:49
+entry_directory =tk .Entry (root )#line:51
+entry_directory .pack ()#line:52
+label_extensions =tk .Label (root ,text ="Enter safe file extensions and separate with a comma (,):")#line:54
+label_extensions .pack ()#line:55
+entry_extensions =tk .Entry (root )#line:57
+entry_extensions .insert (0 ,'.safe')#line:58
+entry_extensions .pack ()#line:59
+label_date =tk .Label (root ,text ="Enter date (MM/DD/YYYY) to delete files created before:")#line:61
+label_date .pack ()#line:62
+default_date =datetime .now ().strftime ('%m/%d/%Y')#line:64
+entry_date =tk .Entry (root )#line:65
+entry_date .insert (0 ,default_date )#line:66
+entry_date .pack ()#line:67
+label_delete =tk .Label (root ,text ="Permanently Delete?")#line:69
+label_delete .pack ()#line:70
+delete_checkbox_var =tk .BooleanVar ()#line:72
+delete_checkbox =tk .Checkbutton (root ,variable =delete_checkbox_var )#line:73
+delete_checkbox .pack ()#line:74
+button =tk .Button (root ,text ="Clear Files",command =clear_files )#line:76
+button .pack ()#line:77
+text =tk .Text (root ,height =10 ,width =40 )#line:79
+text .pack ()#line:80
+root .mainloop ()#line:82
